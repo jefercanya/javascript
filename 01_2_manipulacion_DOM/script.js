1033076f3a98f42996d70640c3b070233d050efe
jefercanya <67928107+jefercanya@users.noreply.github.com>
2022-11-28 18:34:24 -0500

    Update script.js

--- 01_2_manipulacion_DOM/script.js
+++ 01_2_manipulacion_DOM/script.js
@@ -1,6 +1,6 @@
 const h1 = document.querySelector('h1');
 const input1 = document.querySelector('#calculo1');
-const input1 = document.querySelector('#calculo2');
+const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
