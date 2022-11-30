13c0789f5d18f8e09dbe083cec9b6c222a058ce3
jefercanya <67928107+jefercanya@users.noreply.github.com>
2022-11-30 15:09:17 -0500

    Update 08_module.js

--- 03_ECMAScript/src/es6/08_module.js
+++ 03_ECMAScript/src/es6/08_module.js
@@ -1,3 +1,8 @@
+// para usar modulos añadir en -> package.json
+//"author": "Nombre Author <email@example.com>",
+//"license": "MIT",
+//"type": "module" -> Añadir esta linea exactamente
+
 import hello from "./module.js";
 
 hello();
